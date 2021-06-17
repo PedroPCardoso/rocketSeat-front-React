849d37c04bcbdd6fa855278fb538526bc2bfded2
Pedro Cardoso <pedroecomp@gmail.com>
2021-06-17 11:24:58 -0300

    Começo da integração do Backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
 import Header from './components/Header';
 
 import './App.css';
-import bacgroundImage from './assets/background.jpeg';
-function App() {
-    const [projects,setProjects] = useState(['Desenvolvimento de app', "Front-end Web"]);
+import api from'./services/api';
 
+function App() {
+    const [projects,setProjects] = useState([]);
+    useEffect(() => {
+        api.get("projects").then(response => {
+          setProjects(response.data) 
+        });
+
+    },    );
     function HandleAddProject(){
         // projects.push(`Novo projeto ${Date.now()}`);
         setProjects([...projects,`Novo projeto ${Date.now()}`]);
@@ -13,10 +19,9 @@ function App() {
     }
     return (
     <>
-    <img src={bacgroundImage}></img>
     <Header title="Projects"/>
         <ul>
-            {projects.map( project =>  <li key={project}> {project} </li>)}
+            {projects.map( project =>  <li key={project.id}> {project.title} </li>)}
         </ul>
         
         <button type="button" onClick={HandleAddProject}>Adicionar Projeto</button>
